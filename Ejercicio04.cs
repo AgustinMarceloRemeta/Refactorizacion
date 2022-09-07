1b90b7f421b7759c3a116f320cc962f4b6333e6a
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-09-07 02:50:40 -0300

    Update Ejercicio04.cs

--- Ejercicio04.cs
+++ Ejercicio04.cs
@@ -6,6 +6,6 @@ public class Ejercicio04 : MonoBehaviour
     string[] FindEnemys(string[] enemys)
     {
         if (enemys != null) return enemys;
-        return String.Empty;
+        return null;
     }
 }
