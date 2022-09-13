7cf8c60d84a82b789b68d75516f358aa76147227
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-09-13 17:50:32 -0300

    Update Ejercicio04.cs

--- Scrips/Ejercicio04.cs
+++ Scrips/Ejercicio04.cs
@@ -11,16 +11,12 @@ public class Ejercicio04 : MonoBehaviour
     private void Start()
     {
         enemigos = new List<string>() { "Orco", "Araña", "Golem", "Goblin" };
-        enemigo = "Araña"
+        enemigo = "Araña";
     }
 
     string EncontrandoEnemigo()
     {
-        IEnumerable<string> nuevoenemigo = from variable in enemigos
-                                           where variable == enemigo
-                                           select variable;
-
-        if (nuevoenemigo.Contains(enemigo)) return "Se encontro araña";
+        if (enemigos.Contains(enemigo)) return "Se encontro araña";
         else return "No se encontró";
     }
 }
